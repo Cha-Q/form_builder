0454c8b290633ffcfae6350ffdc538276d20169a
Cha-Q <quentin.chassaing@outlook.com>
2023-08-06 14:37:06 +0200

    ajout des éléments radio et select

--- src/Form/Form.php
+++ src/Form/Form.php
@@ -9,8 +9,7 @@
          * @var $fieldName string Form element's name
          */
         private $fieldName;
-
-
+        
 
         
         public function setField(string $fieldName)
@@ -24,41 +23,110 @@
          */
         public function label():string
         {
-            return "<label  for='{$this->fieldName}'>Entrez votre {$this->fieldName}</label><br>";
+            $html = ucfirst($this->fieldName);
+            return "<label  for='{$this->fieldName}'>$html</label>";
         }
 
         /**
-         * @var $field string Value of the new input
+         * @param $field string Name of the new input
+         * @param $type string Value of the input's type
+         * @param $value string Value of the new input
          * @return string
          */
 
-        public function input(string $field):string
+        public function input(string $field, string $type, string $value = null):string
         {
             $this->setField($field);
-            $label = $this->label();
+            $className = 'form-control';
+            $formClass = 'my-3';
+            if($type === 'radio'){
+                $className = 'form-check-input'; 
+                $formClass = '';
+                return "
+                    <div class='form-group $formClass'>
+                        {$this->label()}
+                        <input class=$className type='$type' id='field{$this->fieldName}' name='$value' placeholder='{$this->fieldName}' value={$this->fieldName}>
+                    </div>";
+            }
+            return "
+                <div class='form-group $formClass'>
+                    {$this->label()}
+                    <input class=$className type='$type' id='{$this->fieldName}' name='field{$this->fieldName}' placeholder='Entrez votre {$this->fieldName}' value=$value>
+                </div>";
+        }
+
+        /**
+         * @var $field string Name of the new textarea
+         * @param $type string Value of the input's textarea
+         * @param $value string Value of the new textarea
+         * @return string
+         */
+
+        public function textarea(string $field, string $value = null):string
+        {
+            $this->setField($field);
             return "
             <div class='form-group my-3'>
-                $label
-                <input class='form-control' type='text' id='{$this->fieldName}' name='field{$this->fieldName}' placeholder='{$this->fieldName}' >
+                {$this->label()}
+                <textarea class='form-control' name='field{$this->fieldName}' col='12' row='30' placeholder='Entrez votre {$this->fieldName}'>$value</textarea>
             </div>";
             
         }
 
         /**
-         * @var $field string Value of the new textarea
+         * Generate a select element with options
+         * @param $field string Name of the select's field
+         * @param $options array Names of the options fields and value
          * @return string
          */
 
-        public function textArea(string $field):string
+        public function select(string $field, array $options = [])
         {
             $this->setField($field);
-            $label = $this->label();
             return "
             <div class='form-group my-3'>
-                $label
-                <textarea class='form-control' type='text' name='field{$this->fieldName}' col='12' row='30' placeholder='{$this->fieldName}'></textarea>
+                {$this->label()}
+                <select name='$field' class='form-select'>
+                    {$this->option($options)}
+                </select>
             </div>";
-            
+
+        }
+
+        /**
+         * Generate a select element with options
+         * @param $options array Names of the options fields and values
+         * @return string
+         */
+
+        private function option(array $options = [])
+        {
+            $optionHtml = [];
+            foreach($options as $option){
+                $optionHtml[] = "<option value='$option'>$option</option>";
+            }
+        
+            return implode("<br>",$optionHtml);
+        }
+
+        /**
+         * 
+         * @return string
+         */
+
+        public function radio(array $radios, string $field)
+        {
+            $radioHtml = [];
+            $arrayVal = [];
+            foreach($radios as $k => $radio){
+                $radioHtml[] = 
+                    "<div class='form-check'>
+                        {$this->input($radio, 'radio', $field)}
+                    
+                    
+                    </div>";
+            }
+            return implode("<br>",$radioHtml);;
         }
 
         /**
--- views/homepage.php
+++ views/homepage.php
@@ -4,13 +4,25 @@ use App\Form\Form;
 
 
 $form = new Form();
+
+function stripKeys($replace, $by, $arr){
+    return str_replace($replace, $by, array_keys($arr));
+}
+
+dump(stripKeys('field', '', $_GET));
+// options à passer dans la réalisation de la liste déroulante
+$options = ['a','b','c'];
+
+$radios = ['homme', 'femme', 'vous-même']
 ?>
+
 
 
 <form method="GET" action="">
 
-    <?= $form->input('nom'); ?>
-    <?= $form->textArea('contenu'); ?>
+    <?= $form->input('nom', 'text','Bonjour'); ?>
+    <?= $form->textarea('contenu'); ?>
+    <?= $form->select('catégories', $options) ?>
+    <?= $form->radio($radios, 'gender') ?>
     <?= $form->submit(); ?>
-    
 </form>
